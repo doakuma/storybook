f8367229a10852ec022f25f49a3f3cdcda889007
bp_doakuma <Q!w2e3r4t5>
2020-01-02 23:33:54 +0900

    디자인시스템 구축   - button 디자인 시스템     - 버튼의 상태 : default / Hover / Active / Foucs / Disabled     - 버튼의 theme     - 버튼의 사이즈     - 아이콘과 함께   - React.ReactNode   - @emotion (CSS-in-JS 라이브러리 사용)

---
+++ 01 tutorial/src/Button/Button.stories.tsx
@@ -0,0 +1,56 @@
+/** @jsx jsx */
+import Button from './Button';
+import { jsx, css } from '@emotion/core';
+
+export default {
+    title: 'components|Button',
+    component: Button
+};
+
+export const button = () => {
+    return <Button>BUTTON</Button>
+}
+
+button.story = {
+    name: 'Default'
+};
+
+export const primaryButton = () => {
+    return <Button>PRIMARY</Button>
+};
+export const secondaryButton = () => {
+    return <Button theme="secondary">SECONDARY</Button>
+};
+
+export const tertiaryButton = () => {
+    return <Button theme="tertiary">TERTIARY</Button>
+};
+
+const buttonWrapper = css`
+  .description {
+    margin-bottom: 0.5rem;
+  }
+  & > div + div {
+    margin-top: 2rem;
+  }
+`;
+
+export const sizes = () => {
+    return (
+        <div css={buttonWrapper}>
+            <div>
+                <div className="description">Small</div>
+                <Button size="small">BUTTON</Button>
+            </div>
+            <div>
+                <div className="description">Medium</div>
+                <Button size="medium">BUTTON</Button>
+            </div>
+            <div>
+                <div className="description">Big</div>
+                <Button size="big">BUTTON</Button>
+            </div>
+        </div>
+    );
+};
+
