8cb12f1ecb6206676a72bc7aeca408485d1cb1a5
bp_doakuma <Q!w2e3r4t5>
2020-01-09 21:28:24 +0900

    디자인시스템 구축   - Dialog     - react-spring 이용     - useTransition, animated 확인해보기

--- 01 tutorial/src/Dialog/Dialog.tsx
+++ 01 tutorial/src/Dialog/Dialog.tsx
@@ -3,6 +3,7 @@ import { Fragment } from 'react';
 import { css, jsx } from '@emotion/core';
 import ButtonGroup from '../ButtonGroup/ButtonGroup';
 import Button from '../Button/Button';
+import { useTransition, animated} from "react-spring";
 
 export type DialogProps = {
     visible: boolean;
@@ -29,25 +30,67 @@ const Dialog = ({
     onCancel,
     onConfirm
                 }: DialogProps) => {
-    if (!visible) return null;
+
+    const fadeTransition = useTransition(visible, null, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 }
+    });
+
+    const slideUpTransition = useTransition(visible, null, {
+        from: {
+            transform: `translateY(200px) scale(0.8)`,
+            opacity: 0
+        },
+        enter: {
+            transform: `translateY(0px) scale(1)`,
+            opacity: 1
+        },
+        leave: {
+            transform: `translateY(200px) scale(0.8)`,
+            opacity: 0
+        },
+        config: {
+            tension: 200,
+            friction: 15
+        }
+    });
     return (
         <Fragment>
-            <div css={[fullscreen, darkLayer]}></div>
-            <div css={[fullscreen, whiteBoxWrapper]}>
-                <div css={whiteBox}>
-                    {title && <h3>{title}</h3>}
-                    {description && <p>{description}</p>}
-                    {children}
-                    {!hideButtons && (
-                        <ButtonGroup css={{ marginTop: '3rem' }} rightAlign>
-                            {cancellable && (
-                                <Button theme="tertiary" onClick={onCancel}>{cancelText}</Button>
+            { fadeTransition.map(({ item, key, props}) =>
+                item ? (
+                    <animated.div
+                        css={[fullscreen, darkLayer]}
+                        key={key}
+                        style={props}
+                    >
+                    </animated.div>
+                ) : null
+            )}
+            { slideUpTransition.map(({item, key, props}) =>
+                item ? (
+                    <animated.div
+                        css={[fullscreen, whiteBoxWrapper]}
+                        key={key}
+                        style={props}
+                    >
+                        <div css={whiteBox}>
+                            {title && <h3>{title}</h3>}
+                            {description && <p>{description}</p>}
+                            {children}
+                            {!hideButtons && (
+                                <ButtonGroup css={{ marginTop: '3rem' }} rightAlign>
+                                    {cancellable && (
+                                        <Button theme="tertiary" onClick={onCancel}>{cancelText}</Button>
+                                    )}
+                                    <Button onClick={onConfirm}>{confirmText}</Button>
+                                </ButtonGroup>
                             )}
-                            <Button onClick={onConfirm}>{confirmText}</Button>
-                        </ButtonGroup>
-                    )}
-                </div>
-            </div>
+                        </div>
+                    </animated.div>
+
+                ) : null
+            )}
         </Fragment>
     );
 };
@@ -55,7 +98,7 @@ const Dialog = ({
 Dialog.defaultProps = {
     cancelText: '취소',
     confirmText: '확인'
-}
+};
 
 const fullscreen = css`
   position: fixed;
